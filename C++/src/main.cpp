a1536d994b01a910b569a9ac681ff95331b23c23
Sam - Megatron <none@none>
2018-05-20 16:02:40 -0400

    added method to create superpermutation vertices

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,6 +1,11 @@
 #include "graph.h"
 #include "superperm.h"
 
-int main() {
-  Graph graph = createSuperPermutationGraph(1);
+int main(int argc, char* argv[]){
+  int N = strtol(argv[1], NULL, 0);
+  Graph graph = createSuperPermutationGraph(N);
+  printf("Vertices: %d\r\n", graph.vertices.size());
+  for (int i = 0; i < graph.vertices.size(); i++) {
+    printf("%d: %s\r\n", i, graph.vertices[i]->name.c_str());
+  }
 }
--- C++/src/superperm.cpp
+++ C++/src/superperm.cpp
@@ -1,8 +1,23 @@
 #include "superperm.h"
 
+void createSuperpermVertices(int N, int i, string name, vector<Vertex *> * vertices) {
+  if (i == N) {
+    Vertex * v = new Vertex(name);
+    vertices->push_back(v);
+  } else {
+    for (int j = 0; j < N; j++) {
+      char c = (char)('0' + j);
+      string::size_type index = name.find(c,0);
+      if (index == string::npos) {
+        createSuperpermVertices(N, i+1, name + c, vertices);
+      }
+    }
+  }
+}
 
 vector<Vertex *> createSuperPermutationVertices(int N) {
   vector<Vertex *> vertices;
+  createSuperpermVertices(N, 0, "", &vertices);
   return vertices;
 }
 
@@ -13,7 +28,7 @@ vector<Edge *> createSuperPermutationEdges(vector<Vertex *> vertices) {
 
 Graph createSuperPermutationGraph(int N) {
   Graph graph;
-  graph.vertices = createSuperPermutationVertices(1);
+  graph.vertices = createSuperPermutationVertices(N);
   graph.edges = createSuperPermutationEdges(graph.vertices);
   return graph;
 }
